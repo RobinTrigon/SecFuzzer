b1642001ccbb28bee4e60debb85c599055d62bed
3rr0r-4O4 <86462148+3rr0r-4O4@users.noreply.github.com>
2021-10-06 13:42:36 +0600

    Create SecFuzzer.py

---
+++ SecFuzzer.py
@@ -0,0 +1,41 @@
+#Changing the Description of this tool won't made you the coder ^_^ !
+#Respect Coderz ^_^ 
+
+
+import urllib.request
+import os
+import time
+import sys
+os.system('clear')
+print("""
+\033[96m
+ _____   _   _   _____  _____  _____   ____  
+|  ___| | | | | |__  / |__  / | ____| |  _ \ 
+| |_    | | | |   / /    / /  |  _|   | |_) |
+|  _|   | |_| |  / /_   / /_  | |___  |  _ < 
+|_|      \___/  /____| /____| |_____| |_| \__\
+                                             
+""")
+
+
+url = input("\033[97m [!] Enter URL # ")
+wordlist = input("\033[97m [!] Enter wordlists # ")
+
+def write(word):
+	f1 = open("write1.txt","a")
+	f1.write(word +"\n")
+	
+fo = open(wordlist,"r+")
+for i in range(2000):
+	word = fo.readline(10).strip()
+	surl = url+word
+	#print (surl)
+	
+	response = requests.get(surl)
+	#print (response)
+	if (response.status_code == 200):
+		print ("[+] found :- ",surl)
+		write(word)
+	else:	
+		print ("[-] Not found :- ",surl)
+		pass
