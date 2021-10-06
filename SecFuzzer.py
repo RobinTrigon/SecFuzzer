aee362eb469af32c4dcad764227016dc9c921ac6
3rr0r-4O4 <86462148+3rr0r-4O4@users.noreply.github.com>
2021-10-06 14:12:39 +0600

    Update SecFuzzer.py

--- SecFuzzer.py
+++ SecFuzzer.py
@@ -2,24 +2,44 @@
 #Respect Coderz ^_^ 
 
 
+
 import urllib.request
-import os
+import requests
+import random
 import time
 import sys
+import os
 os.system('clear')
-print("""
-\033[96m
- _____   _   _   _____  _____  _____   ____  
-|  ___| | | | | |__  / |__  / | ____| |  _ \ 
-| |_    | | | |   / /    / /  |  _|   | |_) |
-|  _|   | |_| |  / /_   / /_  | |___  |  _ < 
-|_|      \___/  /____| /____| |_____| |_| \__\
-                                             
-""")
+print('''
+        !!!!!!   !!!!!!!   !!!!!!!  !!!!!!!!  !!!!!!!!             !!    !!  !!!!!!!!  !!    !!
+        !!       !!   !!   !!   !!  !!    !!  !!    !!             !!    !!  !!    !!  !!    !!
+        !!!!!!   !!!!!!!   !!!!!!!  !!    !!  !!!!!!!!   !!!!!     !!!!!!!!  !!    !!  !!!!!!!!
+        !!       !! !!     !! !!    !!    !!  !! !!                      !!  !!    !!        !!
+        !!!!!!   !!  !!    !!  !!   !!!!!!!!  !!  !!                     !!  !!!!!!!!        !!
+''')
+def slowprint(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(5. / 100)
+slowprint("[!] Starting : ")
+time.sleep(1)
+os.system('clear')
+print('''
+\u001b[1;93m
+ ____                  _____                                   
+/ ___|    ___    ___  |  ___|  _   _   ____  ____   ___   _ __ 
+\___ \   / _ \  / __| | |_    | | | | |_  / |_  /  / _ \ | '__|
+ ___) | |  __/ | (__  |  _|   | |_| |  / /   / /  |  __/ | |   
+|____/   \___|  \___| |_|      \__,_| /___| /___|  \___| |_|   
+                                                               3rr0r-404 | fb: @robin2123.py 
+''')
 
 
-url = input("\033[97m [!] Enter URL # ")
-wordlist = input("\033[97m [!] Enter wordlists # ")
+url = input("\033[92m [!] Enter URL : ")
+wordlist = input("\033[92m[!] Enter wordlists : ")
+
+
 
 def write(word):
 	f1 = open("write1.txt","a")
@@ -34,8 +54,6 @@ for i in range(2000):
 	response = requests.get(surl)
 	#print (response)
 	if (response.status_code == 200,403):
-		print ("[+] found :- ",surl)
+		print ("\033[91m[!] Found---> ",surl)
 		write(word)
-	else:	
-		print ("[-] Not found :- ",surl)
-		pass
+
